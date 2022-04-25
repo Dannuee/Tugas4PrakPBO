e16e6bd54dbdddee123c00741cfbdb211ccc45fa
Dannuee <76732501+Dannuee@users.noreply.github.com>
2022-04-25 22:29:20 +0700

    Tugas4PrakPBO

---
+++ src/tugas4pbo_123200025/Main.java
@@ -0,0 +1,16 @@
+package tugas4pbo_123200025;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GUI GUI = new GUI();
+    }
+    
+}
